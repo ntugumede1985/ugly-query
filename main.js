ce578a02f8f4d270112c0106d014643e165fe284
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-08 17:41:10 -0400

    header and font updates

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 const heading = document.querySelector('h1');
 heading.style.fontSize = '100px';
-heading.style.color = 'turquoise';
-//heading.style.
+heading.style.fontFamily = 'Times New Roman';
+heading.style.color = 'rgb(249, 50, 229)';
+heading.style.backgroundImage = "url('https://66.media.tumblr.com/88dba7d7bcbb5c881f332802e0444968/tumblr_moz1bxCW421s0e8ajo1_500.gifv')"
+heading.style.backgroundSize = '200px';
+
+const firstdiv = document.querySelector('#h3heading');
+firstdiv.style.color = 'rgb(242, 167, 237)';
+firstdiv.style.fontSize = '50px';
+
+const seconddiv = document.querySelector('p');
+seconddiv.style.color = 'rgb(207, 166, 234)';
+seconddiv.style.fontSize = '30px';
+
+const thirddiv = document.querySelector('#h3arg');
+thirddiv.style.color = 'rgb(207, 166, 234)';
+thirddiv.style.fontSize = '30px';
+
+const fourthdiv = document.querySelector('#h3quest');
+fourthdiv.style.color = 'rgb(182, 254, 119)';
+fourthdiv.style.fontSize = '15px'
+
+const form = document.querySelector('form');
+form.style.backgroundImage = "url('http://media4.giphy.com/media/l2SpLfkM7QaX1sVAk/giphy.gif')"
 
 document.body.style.backgroundImage = "url('https://www.gunnuts.net/wp-content/uploads/2013/04/20130422-084606.jpg')";
