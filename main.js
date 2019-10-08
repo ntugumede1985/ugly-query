a9db4cda6164f007899c9cc1b7ec316d5f8413e9
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-08 15:33:58 -0400

    background update

---
+++ main.js
@@ -0,0 +1,6 @@
+const heading = document.querySelector('h1');
+heading.style.fontSize = '100px';
+heading.style.color = 'turquoise';
+//heading.style.
+
+document.body.style.backgroundImage = "url('https://www.gunnuts.net/wp-content/uploads/2013/04/20130422-084606.jpg')";
